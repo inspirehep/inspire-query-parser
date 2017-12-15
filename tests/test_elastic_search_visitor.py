ecc45b68160f117f4ee6f68bdc0a7ec8c4a4eade
Chris Aslanoglou <chris.aslanoglou@gmail.com>
2017-12-15 14:47:39 +0100

    es-visitor: check partial BAI on normalized name
    Checking for partial BAI should happen on a normalized author name, so
    that we can make the code that detects it simpler, without adding more
    special cases (addresses inspirehep/inspire-next#3055).
    
    Signed-off-by: Chris Aslanoglou <chris.aslanoglou@gmail.com>

--- tests/test_elastic_search_visitor.py
+++ tests/test_elastic_search_visitor.py
@@ -903,6 +903,27 @@ def test_elastic_search_visitor_queries_also_bai_field_with_wildcard_if_author_n
     assert generated_es_query == expected_es_query
 
 
+def test_elastic_search_visitor_queries_does_not_query_bai_field_if_name_contains_comma_and_dot():
+    query_str = 'a gava,e.'
+
+    generated_es_query = _parse_query(query_str)
+    assert ElasticSearchVisitor.AUTHORS_BAI_FIELD not in str(generated_es_query)
+
+
+def test_elastic_search_visitor_queries_does_not_query_bai_field_if_name_contains_trailing_dot():
+    query_str = 'a mele.'
+
+    generated_es_query = _parse_query(query_str)
+    assert ElasticSearchVisitor.AUTHORS_BAI_FIELD not in str(generated_es_query)
+
+
+def test_elastic_search_visitor_queries_does_not_query_bai_field_if_name_contains_prefix_dot():
+    query_str = 'a .mele'
+
+    generated_es_query = _parse_query(query_str)
+    assert ElasticSearchVisitor.AUTHORS_BAI_FIELD not in str(generated_es_query)
+
+
 def test_elastic_search_visitor_does_not_query_bai_field_if_name_contains_dot_and_spaces():
     query_str = 'a S. Mele'
     bai_field = "authors.ids.value.search"
